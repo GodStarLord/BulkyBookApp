eb82a8d169a4ce44b824f4424ebdf5edee514c08
GodStarLord <62302642+GodStarLord@users.noreply.github.com>
2022-05-08 15:00:05 +0530

    Add ApplicationDbContext

---
+++ BulkyBookApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkyBookApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+ 
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
