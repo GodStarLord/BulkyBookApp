c424e76afc28646c4e1bd8164cf0bbd4f798803d
GodStarLord <62302642+GodStarLord@users.noreply.github.com>
2022-05-08 15:24:10 +0530

    Retrieve all Categories

--- BulkyBookApp/Controllers/CategoryController.cs
+++ BulkyBookApp/Controllers/CategoryController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBookApp.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookApp.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext applicationContext)
+        {
+            _context = applicationContext;
+        }
+
         public IActionResult Index()
         {
+            var objCategoryList = _context.Categories.ToList();
             return View();
         }
     }
