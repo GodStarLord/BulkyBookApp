1939b26823a542741e6b04880b167ee3b3d4786c
GodStarLord <62302642+GodStarLord@users.noreply.github.com>
2022-05-12 16:14:02 +0530

    Implement IRepository
    Implemente IRepository Class

---
+++ BulkyBook.DataAccess/Repositories/Implementations/Repository.cs
@@ -0,0 +1,54 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repositories.Implementations
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        internal DbSet<T> _dbSet;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = _dbSet;
+            return query.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = _dbSet;
+
+            query = query.Where(filter);
+
+            return query.FirstOrDefault();
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+    }
+}
