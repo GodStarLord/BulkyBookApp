37695e538b84bbdb6e631151d460e4069098488d
GodStarLord <62302642+GodStarLord@users.noreply.github.com>
2022-05-12 16:03:36 +0530

    IRepository Interface
    Implement Generic Repository Interface

---
+++ BulkyBook.DataAccess/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repositories.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        // T - Generic Class Type
+
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
