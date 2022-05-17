23519d325eed3d998e36a7ccf71eace9aa474a0c
GodStarLord <62302642+GodStarLord@users.noreply.github.com>
2022-05-17 12:51:56 +0530

    What got Added with Identity
    Resolve Errors with identity Scaffolding
    and add partial view

--- BulkyBookApp/Program.cs
+++ BulkyBookApp/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
